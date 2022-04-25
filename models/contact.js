e1097711bdb9ca8be6f5672c1104c8fc758feee3
RaihanStark <raihanyudosaputra30@gmail.com>
2022-04-25 10:46:49 +0700

    Add: Phone Validators

--- models/contact.js
+++ models/contact.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
-
 const URI = process.env.MONGODB_URI;
 
+// Initiate Mongoose
 mongoose
   .connect(URI)
   .then(() => {
@@ -13,13 +13,24 @@ mongoose
     console.log(`error connecting to MongoDB: ${error.message}`);
   });
 
+// Schema
 const contactSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true, minLength: 3 },
-  phoneNumber: { type: String, required: true, unique: false, minLength: 8 },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: false,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{1,}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 contactSchema.plugin(uniqueValidator);
-
 contactSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
